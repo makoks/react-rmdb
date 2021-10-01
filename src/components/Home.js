75810728c40ab852a43b61fdd12ef272ca1b9d3a
makoks <kolesoks@gmail.com>
2021-10-01 16:07:09 +0500

    feat: fetch movies for page 1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+// API
+import API from '../API';
 
 // Config
 import { IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../config';
@@ -10,6 +13,26 @@ const Home = () => {
     const [state, setState] = useState();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+
+    const fetchMovies = async (page, searchTerm = '') => {
+        try {
+            setError(false);
+            setLoading(true);
+
+            const movies = await API.fetchMovies(searchTerm, page);
+            setState(prev => ({
+                ...movies,
+                results: (page > 1) ? [...prev.results, ...movies.results] : [...movies.results]
+            }));
+
+        } catch(error) {
+            setError(true);
+        }
+
+        setLoading(false);
+    };
+
+    useEffect(() => fetchMovies(1), []);
 
     return <div>Home page</div>;
 };
