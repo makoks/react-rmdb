dbd45aee0ff684d84c9bdad8ed79651bfc14ef9b
makoks <kolesoks@gmail.com>
2021-10-04 22:43:00 +0500

    feat: add hero image

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,9 @@ import React from 'react';
 // Config
 import { IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../config';
 
+// Components
+import HeroImage from './HeroImage';
+
 // Hooks
 import { useHomeFetch } from '../hooks/useHomeFetch';
 
@@ -12,7 +15,17 @@ import NoImage from '../images/no_image.jpg';
 const Home = () => {
     const { state, loading, error } = useHomeFetch();
 
-    return <div>Home page</div>;
+    return (
+        <>
+            {state.results[0] &&
+                <HeroImage
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                    title={state.results[0].title}
+                    text={state.results[0].overview}
+                />
+            }
+        </>
+    );
 };
 
 export default Home;
