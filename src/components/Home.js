4ac0a67f62cd17395ed6bf7373cb4f71aa4dc05d
makoks <kolesoks@gmail.com>
2021-10-04 17:38:11 +0500

    refactor: move fetch logic from home component to custom hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,16 @@
-import React, { useEffect, useState } from 'react';
-
-// API
-import API from '../API';
+import React from 'react';
 
 // Config
 import { IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../config';
+
+// Hooks
+import { useHomeFetch } from '../hooks/useHomeFetch';
 
 // Images
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-
-    const fetchMovies = async (page, searchTerm = '') => {
-        try {
-            setError(false);
-            setLoading(true);
-
-            const movies = await API.fetchMovies(searchTerm, page);
-            setState(prev => ({
-                ...movies,
-                results: (page > 1) ? [...prev.results, ...movies.results] : [...movies.results]
-            }));
-
-        } catch(error) {
-            setError(true);
-        }
-
-        setLoading(false);
-    };
-
-    useEffect(() => fetchMovies(1), []);
+    const { state, loading, error } = useHomeFetch();
 
     return <div>Home page</div>;
 };
