ce6fa76b19fe9e5cb3a329c00dc475adf93b17df
makoks <kolesoks@gmail.com>
2021-10-05 16:34:18 +0500

    feat: add button

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ import SearchBar from './SearchBar';
 import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
+import Button from './Button';
 
 // Hooks
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -44,7 +45,10 @@ const Home = () => {
                     />
                 ))}
             </Grid>
-            <Spinner />
+
+            {loading && <Spinner />}
+
+            {state.page < state.total_pages && !loading && <Button text='Load More' />}
         </>
     );
 };
