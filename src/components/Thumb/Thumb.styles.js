9e5c85bf49984a89e8460b0adc0c212ee7e8eff3
makoks <kolesoks@gmail.com>
2021-10-05 13:45:45 +0500

    feat: style thumbnails

--- src/components/Thumb/Thumb.styles.js
+++ src/components/Thumb/Thumb.styles.js
@@ -1,4 +1,20 @@
 import styled from 'styled-components';
 
 export const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    max-width: 720px;
+    object-fit: cover;
+    border-radius: 20px;
+    transition: all 0.3s;
+    animation: animateThumb 0.5s;
+
+    :hover {
+        opacity: 0.8;
+    }
+
+    @keyframes animateThumb {
+        from {opacity: 0;}
+        to {opacity: 1;}
+    }
 `;
