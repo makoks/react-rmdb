64afa076fc079293b8d851b38f0d4e01b86e8a91
makoks <kolesoks@gmail.com>
2021-10-18 19:51:41 +0500

    feat: add routing (home and movie)

--- src/components/Thumb/index.js
+++ src/components/Thumb/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Styles
 import { Image } from './Thumb.styles';
 
-const Thumb = ({ image, movieId, clickable }) => (
-    <div>
-        <Image src={image} alt='movie-poster' />
-    </div>
-);
+const Thumb = ({ image, movieId, clickable }) => {
+    return clickable ? (
+        <div>
+            <Link to={`/${movieId}`}>
+                <Image src={image} alt='movie-poster' />
+            </Link>
+        </div>
+    ) : (
+        <div>
+            <Image src={image} alt='movie-poster' />
+        </div>
+    );
+};
 
 export default Thumb;
